364554ef5d48190d9e1934aa06dfdcdab43d9863
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2024-04-05 21:25:39 +0530

    completed Interation

--- web-app/app/frames-api/[cid]/Ad/[adId]/route.tsx
+++ web-app/app/frames-api/[cid]/Ad/[adId]/route.tsx
@@ -2,18 +2,37 @@
 import { frames } from "../../frames";
 import { Button } from "frames.js/next";
 import { redirect } from "frames.js/core";
+import { Address, ABI } from "@/Constants/ContractDetails";
+import { Wallet, JsonRpcProvider, Contract } from "ethers";
 
-export const POST = frames(async (ctx) => {
-  console.log(ctx.url.pathname);
+export const POST = frames(async ({ ctx, state, url, searchParams }) => {
+  const path = url.pathname;
+  console.log(state.AdDestinataion);
+  const provider = new JsonRpcProvider(
+    process.env.NEXT_PUBLIC_BASE_SEPOLIA_RPC_URL
+  );
+  if (!process.env.NEXT_PUBLIC_PRIVATE_KEY) {
+    throw new Error("Private key is not defined");
+  }
+  const signer = new Wallet(process.env.NEXT_PUBLIC_PRIVATE_KEY, provider);
+  const frameId = path.split("/")[2];
+  const AdId = path.split("/")[4];
+  const contract = new Contract(Address, ABI, signer);
+  let tx = await contract.serveAd(AdId, frameId);
+  const goBackTarget = `/${frameId}`;
+  const imgSrc = `${process.env.NEXT_PUBLIC_PINATA_GATEWAY}/ipfs/${searchParams.adImage}`;
   if (ctx.pressedButton?.action === "post_redirect") {
-    // Do something with the POST
-    // when post_redirect button is clicked you must return a redirect response
-    return redirect("https://google.com");
+    await tx.wait();
+    tx = await contract.transferClickReward(AdId, frameId);
+    console.log(ctx.searchParams.adUrl);
+    return redirect(
+      `${process.env.NEXT_PUBLIC_PINATA_GATEWAY}/ipfs/${ctx.searchParams.adUrl}`
+    );
   }
   return {
-    image: <div tw="flex">Advertisement</div>,
+    image: imgSrc,
     buttons: [
-      <Button action="post" target="/">
+      <Button action="post" target={goBackTarget}>
         Go Back
       </Button>,
       <Button action="post_redirect">Visit</Button>,
